8393f2b7e875167a6ad5b086f5e067a30035d597
Denilson Makotsi <denilsonmakotsi@gmail.com>
2019-03-29 15:15:10 +0300

    removed the upper case Q to make my code uniform.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event) {
     event.preventDefault();
-    var shortForm = parseInt($("input:radio[name=Quiz1]:checked").val());
+    var shortForm = parseInt($("input:radio[name=quiz1]:checked").val());
     var js = parseInt($("input:radio[name=quiz2]:checked").val());
-    var language = parseInt($("input:radio[name=Quiz3]:checked").val());
+    var language = parseInt($("input:radio[name=quiz3]:checked").val());
     var display = parseInt((shortForm + js + language) / 20 * 100);
     $("hide").slideDown();
-    $("#results").text("your score is :" + " " +display + "%");
+    $("#results").text("your score is : "+display + "%");
 
 
 
