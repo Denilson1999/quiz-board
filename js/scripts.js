f74568439023b07261d292f5e0ad7b9ce8d1cb10
Denilson Makotsi <denilsonmakotsi@gmail.com>
2019-03-29 15:03:23 +0300

    beautified the file.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-$("#quiz").submit(function(event){
-  event.preventDefault();
-     var shortForm = parseInt($("input:radio[name=Quiz1]:checked").val());
-     var js = parseInt($("input:radio[name=quiz2]:checked").val());
-     var language = parseInt($("input:radio[name=Quiz3]:checked").val());
-     var display = parseInt((shortForm + js + language) /20*100);
-      $("hide").slideDown();
-      $("#results").text("your score is :"+" "+display + "%");
-      
+$(document).ready(function() {
+  $("#quiz").submit(function(event) {
+    event.preventDefault();
+    var shortForm = parseInt($("input:radio[name=Quiz1]:checked").val());
+    var js = parseInt($("input:radio[name=quiz2]:checked").val());
+    var language = parseInt($("input:radio[name=Quiz3]:checked").val());
+    var display = parseInt((shortForm + js + language) / 20 * 100);
+    $("hide").slideDown();
+    $("#results").text("your score is :" + " " +display + "%");
 
 
 
 
 
-})
+
+  })
 
 })
