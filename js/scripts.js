1bd0c63bf7cb160d6477105cbd2a2514dbf86e08
Denilson Makotsi <denilsonmakotsi@gmail.com>
2019-03-29 17:22:53 +0300

    used the prevent default.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("#quiz").submit(function(event) {
-    event.preventDefault();
+
     var shortForm = parseInt($("input:radio[name=quiz1]:checked").val());
     var js = parseInt($("input:radio[name=quiz2]:checked").val());
     var language = parseInt($("input:radio[name=quiz3]:checked").val());
-    var display = parseInt((shortForm + js + language) / 20 * 100);
+    var booleans = parseInt($("input:radio[name=quiz4]:checked").val());
+    var display = parseInt((shortForm + js + language + booleans) / 30 * 100);
+      event.preventDefault();
     $("hide").slideDown();
     $("#results").text("your score is : "+display + "%");
 
